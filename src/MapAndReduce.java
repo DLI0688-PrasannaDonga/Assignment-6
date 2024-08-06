945bfcdab688ef57f953ab1391b004ce797a8c7a
Prasanna Donga <Prasanna.Donga@godigit.com>
2024-08-06 09:54:51 +0530

    map and reduce

--- src/MapAndReduce.java
+++ src/MapAndReduce.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class MapAndReduce {
     public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(4,6,3,9);
+        Optional<Integer> fine=list.stream().filter(n->n%2==0).map(n->n*n).reduce((a, b)->{
+            return (a+b);
+        });
+
+        System.out.println(fine.get());
 
     }
 }
